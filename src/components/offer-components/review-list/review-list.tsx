3fe3c063bdd78cce02ac48964749f6a9108c13ed
macl73 <la-paev@yandex.ru>
2023-11-07 22:42:50 +0400

    Введены сортировка и лимит отзывов

--- src/components/offer-components/review-list/review-list.tsx
+++ src/components/offer-components/review-list/review-list.tsx
@@ -1,15 +1,17 @@
 import Review from '../review/review.tsx';
 import ReviewForm from '../review-form/review-form.tsx';
-import { TReviewList } from '../../../types/index.ts';
+import { TReview, TReviewList } from '../../../types/index.ts';
 import { AuthorizationStatus } from '../../../const.ts';
 
 export default function ReviewList(props: TReviewList): JSX.Element {
+
+  const reviews: TReview[] = props.reviews.sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date))).slice(0, 10);
 
   return (
     <section className="offer__reviews reviews">
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{props.reviews.length}</span></h2>
       <ul className="reviews__list">
-        {props.reviews.map((item) => (
+        {reviews.map((item) => (
           <Review
             key={item.id}
             review={item}
