cd88572c5b745f0eed6654bfc8c8f238aff78772
macl73 <la-paev@yandex.ru>
2023-11-08 19:04:36 +0400

    Добавлен и подключен компонент ScrollToTop

--- src/const.ts
+++ src/const.ts
@@ -15,7 +15,7 @@ export enum AuthorizationStatus {
 export const ConstantValues = {
   MinCommentLength: 50,
   MaxCommentLength: 300,
-};
+} as const;
 
 export const PIN_MARKER_DEFAULT = 'img/pin.svg';
 
