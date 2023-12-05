c09cbfcc771dcde523fe959a5fd0d1231059ca08
macl73 <la-paev@yandex.ru>
2023-12-05 23:59:53 +0400

    Добавлен переход на логин после выхода из учетной записи

--- src/components/offer-components/rating/rating.tsx
+++ src/components/offer-components/rating/rating.tsx
@@ -5,16 +5,17 @@ import { STARS } from '../../../const';
 export type TStarsCount = {
   starsCount: (evt: ChangeEvent<HTMLInputElement>) => void;
   rating: number;
+  formBlocking: boolean;
 };
 
-export function Rating({starsCount, rating}: TStarsCount): JSX.Element {
+export function Rating({starsCount, rating, formBlocking}: TStarsCount): JSX.Element {
 
   return (
     <div className="reviews__rating-form form__rating">
       {STARS.map((star) => (
         <Fragment key={star.id}>
-          <input className="form__rating-input visually-hidden" onChange={(e) => starsCount(e)} name="rating" value={star.id} id={`${star.id}-star`} type="radio" checked={star.id <= rating} />
-          <label htmlFor={`${star.id}-star`} className="reviews__rating-label form__rating-label" title={star.title}>
+          <input className="form__rating-input visually-hidden" onChange={starsCount} name="rating" value={star.id} id={`${star.id}-stars`} type="radio" checked={star.id === rating} disabled={formBlocking} />
+          <label htmlFor={`${star.id}-stars`} className="reviews__rating-label form__rating-label" title={star.title}>
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
             </svg>
